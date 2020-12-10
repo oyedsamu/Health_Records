7b3368c3c574b786fa5ff0f94d5fac206d2e3b57
timkwali <46701145+timkwali@users.noreply.github.com>
2020-12-10 13:47:26 +0100

    add bottom nav menu to page

--- app/src/main/java/com/decadevs/healthrecords/MainActivity.kt
+++ app/src/main/java/com/decadevs/healthrecords/MainActivity.kt
@@ -3,6 +3,7 @@ package com.decadevs.healthrecords
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.decadevs.healthrecords.ui.doctorpage.DoctorPageFragment
+import com.decadevs.healthrecords.ui.patientdetails.PatientDetailsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +11,9 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.Theme_HealthRecords)
         setContentView(R.layout.activity_main)
 
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.framelayout, PatientDetailsFragment())
+                .commit()
+        }
     }
 }
