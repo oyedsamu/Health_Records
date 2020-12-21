9f8ca04ef65fa8eb0db3172e461074dce6811683
Daniel Ayodeji <danielayodeji97@gmail.com>
2020-12-21 07:02:15 +0100

    added adapter for dropdown

--- app/src/main/java/com/decadevs/healthrecords/ui/prescription/DoctorPrescriptionFragment.kt
+++ app/src/main/java/com/decadevs/healthrecords/ui/prescription/DoctorPrescriptionFragment.kt
@@ -5,18 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.decadevs.healthrecords.R
+import com.decadevs.healthrecords.databinding.FragmentDoctorPrescriptionBinding
 
 
 class DoctorPrescriptionFragment : Fragment() {
 
+    private var _binding : FragmentDoctorPrescriptionBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doctor_prescription, container, false)
+        _binding = FragmentDoctorPrescriptionBinding.inflate(inflater, container, false)
+
+        val items = listOf<String>("Group A", "Group B", "Group AB", "Group O")
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_item_dropdown, items)
+
+        return binding.root
     }
 
 }
