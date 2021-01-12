5657b36e1ea08dc50543c63c1214a456010d3310
Daniel Ayodeji <danielayodeji97@gmail.com>
2021-01-12 10:20:43 +0100

    set up network module

---
+++ app/src/main/java/com/decadevs/healthrecords/di/NetworkModule.kt
@@ -0,0 +1,58 @@
+package com.decadevs.healthrecords.di
+
+import com.decadevs.healthrecords.data.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideLogger () : HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory () : Converter.Factory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideClient (logger : HttpLoggingInterceptor) : OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideService (client: OkHttpClient, converterFactory : Converter.Factory) : Retrofit {
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(converterFactory)
+            .build()
+
+    }
+
+    @Provides
+    @Singleton
+    fun provideLoginApiService (retrofit: Retrofit) {
+
+    }
+
+}
