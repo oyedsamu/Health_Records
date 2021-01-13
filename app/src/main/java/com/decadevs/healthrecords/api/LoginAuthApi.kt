8e0e470e23a180d0015ce5f018c46dcc34dd75c9
dikascode <dikaemanuel@gmail.com>
2021-01-13 12:53:27 +0100

    extended baserepository, HRrepository and modified loginauth api

--- app/src/main/java/com/decadevs/healthrecords/api/LoginAuthApi.kt
+++ app/src/main/java/com/decadevs/healthrecords/api/LoginAuthApi.kt
@@ -1,6 +1,8 @@
 package com.decadevs.healthrecords.api
 
+import com.decadevs.healthrecords.model.GenericResponseClass
 import com.decadevs.healthrecords.model.LoginRequest
+import com.decadevs.healthrecords.model.LoginResponse
 import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
@@ -10,8 +12,8 @@ interface LoginAuthApi {
 
 
     @POST("Auth/login")
-    suspend fun login (
-        @Body loginRequest : LoginRequest
-    )
+    suspend fun login(
+        @Body loginRequest: LoginRequest
+    ): GenericResponseClass<LoginResponse>
 
 }
