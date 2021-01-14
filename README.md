a64d1fac020d60146f2d85fb157da89189c5d99f
Oyedele Samuel <oyedsamu@gmail.com>
2021-01-14 12:19:30 +0100

    Nurse Comments Screen done

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A National Health Directory project built by DecaDevs on Internship with Access 
 This project will make it possible for Hospitals and other Heath centres in the country to easily collaborate in taking care of patients by having easy and secure access to patients' medical records. 
 
 The benefits of this project includes:
-- A National Healthcare database.
+- A National HealthCare database.
 - Reduce unnecessary testing whenever a patient needs to be treated in a new health facility.
 - Easy management of patients' information.
 - Prevention of wrong medication as allergies and previous medical records are used before and during treatment.
---
+++ app/src/main/java/com/decadevs/healthrecords/ui/NurseComments.kt
@@ -0,0 +1,30 @@
+package com.decadevs.healthrecords.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.decadevs.healthrecords.R
+import com.decadevs.healthrecords.databinding.FragmentNurseCommentsBinding
+import com.decadevs.healthrecords.databinding.FragmentPatientDetailsBinding
+
+
+class NurseComments : Fragment() {
+    private var _binding: FragmentNurseCommentsBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+      _binding = FragmentNurseCommentsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
