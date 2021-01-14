b6f2f96fa06bc57908a2ae52a7653c6440ace2a6
dikascode <dikaemanuel@gmail.com>
2021-01-14 01:28:54 +0100

    Implemented remember me

--- app/src/main/java/com/decadevs/healthrecords/ui/LoginFragment.kt
+++ app/src/main/java/com/decadevs/healthrecords/ui/LoginFragment.kt
@@ -6,20 +6,26 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
 import androidx.navigation.fragment.findNavController
 import com.decadevs.healthrecords.R
 import com.decadevs.healthrecords.api.LoginAuthApi
 import com.decadevs.healthrecords.api.Resource
 import com.decadevs.healthrecords.databinding.FragmentLoginBinding
+import com.decadevs.healthrecords.datastore.UserManager
 import com.decadevs.healthrecords.model.LoginRequest
 import com.decadevs.healthrecords.repository.HealthRecordsRepositoryImpl
 import com.decadevs.healthrecords.viewmodel.HealthRecordsViewModel
 import com.decadevs.healthrecords.viewmodel.ViewModelFactory
 import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -32,6 +38,9 @@ class LoginFragment : Fragment() {
 
     private lateinit var viewModel: HealthRecordsViewModel
     lateinit var progressBar: ProgressBar
+    lateinit var rememberMe: CheckBox
+    lateinit var userManager: UserManager
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,6 +51,7 @@ class LoginFragment : Fragment() {
 
         val uID = binding.uniqueIdEditText
         val pwd = binding.passwordEditText
+        rememberMe = binding.rememberMe
 
         progressBar = binding.progressBarLayout.fragmentMainProgressBar
 
@@ -49,29 +59,34 @@ class LoginFragment : Fragment() {
         val factory = ViewModelFactory(repository)
 
         viewModel = ViewModelProvider(this, factory).get(HealthRecordsViewModel::class.java)
+        userManager = UserManager(requireActivity())
 
-        viewModel.loginResponse.observe(viewLifecycleOwner, {
-            Log.i("Login Response ", "$it")
-
-            when (it) {
-                is Resource.Success -> {
-                    val successResponse = it.value.message
-                    Log.i("Login Response", "$successResponse")
-                    progressBar.visibility = View.GONE
-                    findNavController().navigate(R.id.doctorPageFragment)
-                }
-                is Resource.Failure -> {
-                    Log.i("Login Response Failure", "${it.errorBody}, ${it.isNetworkError}")
-                    progressBar.visibility = View.GONE
-                }
-            }
-
-        })
-
+        /**
+         * Set uid and password into login edit fields if rememberMe already saved
+         */
+        checkAndImplementRememberMe(pwd, uID)
 
         binding.signInButton.setOnClickListener {
             progressBar.visibility = View.VISIBLE
             validateLoginInput(uID, pwd)
+
+            viewModel.loginResponse.observe(viewLifecycleOwner, {
+                Log.i("Login Response ", "$it")
+
+                when (it) {
+                    is Resource.Success -> {
+                        val successResponse = it.value.message
+                        Log.i("Login Response", "$successResponse")
+                        progressBar.visibility = View.GONE
+                        findNavController().navigate(R.id.doctorPageFragment)
+                    }
+                    is Resource.Failure -> {
+                        Log.i("Login Response Failure", "${it.errorBody}, ${it.isNetworkError}")
+                        progressBar.visibility = View.GONE
+                    }
+                }
+
+            })
         }
 
         return binding.root
@@ -91,9 +106,36 @@ class LoginFragment : Fragment() {
             else -> {
                 val loginRequest = LoginRequest(uID.text.toString(), pwd.text.toString())
                 viewModel.login(loginRequest)
+
+                if (rememberMe.isChecked) {
+                    //Save user login data to DataStore
+                    GlobalScope.launch {
+                        userManager.createRememberMeSession(
+                            uID.text.toString(),
+                            pwd.text.toString()
+                        )
+                    }
+                }
             }
         }
     }
+
+
+    private fun checkAndImplementRememberMe(password: EditText, uID: EditText) {
+        userManager.rmUserIdFlow.asLiveData().observe(requireActivity(), { uid ->
+            if (uid != "") {
+                uID.setText(uid)
+                rememberMe.isChecked = true
+            }
+        })
+
+        userManager.rmUserPasswordFlow.asLiveData().observe(requireActivity(), { pwd ->
+            if (pwd != "") {
+                password.setText(pwd)
+            }
+        })
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
