91c7bb199c313038c0d9d4102febed40cd3217c2
Oyedele Samuel <oyedsamu@gmail.com>
2021-01-15 13:38:39 +0100

    Patient Registration Form completed

---
+++ app/src/main/java/com/decadevs/healthrecords/ui/institution/InstitutionRegistration.kt
@@ -0,0 +1,24 @@
+package com.decadevs.healthrecords.ui.institution
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.decadevs.healthrecords.R
+
+class InstitutionRegistration : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_institution_registration, container, false)
+    }
+}
