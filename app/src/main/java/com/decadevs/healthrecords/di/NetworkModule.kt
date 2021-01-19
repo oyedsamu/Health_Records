1b9e7c99669dc941594a8599fea258dfc701e456
dikascode <dikaemanuel@gmail.com>
2021-01-19 13:24:04 +0100

    Edited timeout for okhttp client connection

--- app/src/main/java/com/decadevs/healthrecords/di/NetworkModule.kt
+++ app/src/main/java/com/decadevs/healthrecords/di/NetworkModule.kt
@@ -36,9 +36,9 @@ object NetworkModule {
     fun provideClient (logger : HttpLoggingInterceptor) : OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(logger)
-            .connectTimeout(10, TimeUnit.SECONDS)
-            .readTimeout(10, TimeUnit.SECONDS)
-            .writeTimeout(10, TimeUnit.SECONDS)
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .readTimeout(5, TimeUnit.SECONDS)
+            .writeTimeout(5, TimeUnit.SECONDS)
             .build()
     }
 
