124a1a12f84dcd29532b39dd76b8fb727c783809
Daniel Ayodeji <danielayodeji97@gmail.com>
2021-02-07 19:27:02 +0100

    make changes

--- app/src/main/java/com/decadevs/healthrecords/ui/LoginFragment.kt
+++ app/src/main/java/com/decadevs/healthrecords/ui/LoginFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.asLiveData
@@ -92,6 +93,7 @@ class LoginFragment : Fragment() {
                     is Resource.Failure -> {
                         Log.i("Login Response Failure", "${it.errorBody}, ${it.isNetworkError}")
                         progressBar.visibility = View.GONE
+                        Toast.makeText(requireContext(), "Invalid UniqueID or Password", Toast.LENGTH_LONG).show()
                     }
                 }
 
