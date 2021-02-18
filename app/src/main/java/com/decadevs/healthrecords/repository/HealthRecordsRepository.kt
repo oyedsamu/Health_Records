ced5ae9b992a3f5f19ea188306d3652ef3cdaa70
dikascode <dikaemanuel@gmail.com>
2021-02-18 15:19:39 +0100

    repository and impl set up for update record

--- app/src/main/java/com/decadevs/healthrecords/repository/HealthRecordsRepository.kt
+++ app/src/main/java/com/decadevs/healthrecords/repository/HealthRecordsRepository.kt
@@ -2,10 +2,7 @@ package com.decadevs.healthrecords.repository
 
 import android.app.Activity
 import com.decadevs.healthrecords.api.Resource
-import com.decadevs.healthrecords.model.request.ForgotPwdRequest
-import com.decadevs.healthrecords.model.request.LoginRequest
-import com.decadevs.healthrecords.model.request.MedicalRecordRequest
-import com.decadevs.healthrecords.model.request.ResetPasswordRequest
+import com.decadevs.healthrecords.model.request.*
 import com.decadevs.healthrecords.model.response.LoginResponse
 import com.decadevs.healthrecords.model.response.MedicalRecordResponse
 import com.decadevs.healthrecords.model.response.StaffResponse
@@ -21,12 +18,20 @@ interface HealthRecordsRepository {
 
     suspend fun resetPassword(resetPasswordRequest: ResetPasswordRequest): Resource<Any>
 
-    suspend fun addMedicalRecord(token : String, activity: Activity, diagnosis : String,
-                                 prescription : String, isSensitive : Boolean, doctorNotes : String,
-                                 patientRegistrationNumber : String,  documentFormFiles : String,
-                                 documentDescription : String ): Resource<GenericResponseClass<FormData>>
+    suspend fun addMedicalRecord(
+        token: String, activity: Activity, diagnosis: String,
+        prescription: String, isSensitive: Boolean, doctorNotes: String,
+        patientRegistrationNumber: String, documentFormFiles: String,
+        documentDescription: String
+    ): Resource<GenericResponseClass<FormData>>
 
     suspend fun getPatientAllRecords(patientId: String): Resource<PatientAllRecordsResponse>
 
     suspend fun getPatientData(patientId: String): Resource<PatientResponse>
+
+    suspend fun updatePatientRecord(
+        token: String,
+        patientRegistrationNumber: String,
+        medicalRecordUpdateRequest: RecordUpdateRequest
+    ): Resource<Any>
 }
